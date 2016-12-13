d1eec453ec33df4cdbb3ee33271eecc917e80471
adam-zhang <mingpingzhang@163.com>
2016-12-13 14:08:38 +0800

    it lets program be normal

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
 	if (argc < 2)
 	{
 		printf("arguments count is not right.\n");
 		return 0;
 	}
-	printf("before fopen.\n");
-	FILE* file = fopen("file.log", "wa");
-	printf("after fopen.\n");
+	FILE* file = fopen("file.log", "a+");
 	size_t length = strlen(argv[1]);
-	printf("length:%ld\n", length);
-	//fwrite(argv[1], sizeof(char), length, file);
-	//fwrite("\n", sizeof(char), 1, file);
+	fwrite(argv[1], sizeof(char), length, file);
+	fwrite("\n", sizeof(char), 1, file);
 	fclose(file);
 	return 0;
 }
